ce80d09f0620e6f08fbb9cd73c66ce4f853c4f7e
liuzj <liuzj039@gmail.com>
2020-12-02 10:17:08 +0800

    add polyACaller ; add kb use func to singlecellJupytertools

--- tools/singleCell/jupyterUseTempTools.py
+++ tools/singleCell/jupyterUseTempTools.py
@@ -6,13 +6,17 @@ import seaborn as sns
 import anndata
 from scipy.stats import spearmanr, pearsonr, zscore
 from loguru import logger
+from io import StringIO
 from concurrent.futures import ProcessPoolExecutor as Mtp
 import sh
+import h5py
 
 
 #################
 ##一些简单的工具###
 #################
+
+
 
 def mkdir(dirPath):
     try:
@@ -57,19 +61,27 @@ def plotCellScatter(adata):
     ]
 
     
-def creatAnndataFromDf(df):
+def creatAnndataFromDf(df, **layerInfoDt):
     """
     dataframe转换成anndata
+    df,
+    layerInfoDt:
+        key为layer名
+        value为mtx
+    均行为barcode 列为feature 维度相同
     """
     transformedAd = anndata.AnnData(
         X=df.values,
         obs=pd.DataFrame(index = df.index),
         var=pd.DataFrame(index = df.columns)
     )
+    for layerName, layerMtx in layerInfoDt.items():
+        transformedAd.layers[layerName] = layerMtx
+        
     return transformedAd
 
 
-def mergeAdataExpress(adata, groupby='louvain', useRaw=True, logTransformed=True):
+def mergeAdataExpress(adata, groupby='louvain', useRaw=True, logTransformed=True, method='mean'):
     """
     通过adata.obs中的<groupby>对表达量合并
     """
@@ -85,7 +97,7 @@ def mergeAdataExpress(adata, groupby='louvain', useRaw=True, logTransformed=True
     adataExpressDf = pd.DataFrame(adata.raw.X.A, columns=adata.raw.var.index, index=adata.obs.index) if useRaw else adata.to_df()
     adataExpressDf = np.exp(adataExpressDf) - 1 if logTransformed else adataExpressDf
 
-    groupbyExpressDf = adataExpressDf.join(adata.obs[groupby]).groupby(groupby).agg('mean')
+    groupbyExpressDf = adataExpressDf.join(adata.obs[groupby]).groupby(groupby).agg(method)
     groupbyExpressDf = np.log(groupbyExpressDf + 1) if logTransformed else groupbyExpressDf
 
     groupbyExpressAd = __creatAnndataFromDf(groupbyExpressDf)
@@ -669,3 +681,264 @@ def combineAdataUseScanorama(adataLs, batchKey, batchCateLs, subSample=False, su
     sc.tl.umap(combineAdata)
 
     return combineAdata
+
+
+##########
+##bustools
+##########
+def parseBustoolsIndex(t2gPath, t2cPath=False):
+    """
+    解析bustools的索引文件
+    
+    t2gPath:
+        kbpython index t2g
+    
+    return:
+        t2gDt: key为转录本名 value为基因名
+        trLs: 所有的转录本 sorted
+        geneLs: 所有的基因 sorted
+    """
+    t2cTrList = []
+    if not t2cPath:
+        pass
+    else:
+        with open(t2cPath) as t2cFh:
+            for line in t2cFh:
+                t2cTrList.append(line.strip())
+    t2cTrSet = set(t2cTrList)
+    
+    t2gDt = {}
+    trLs = []
+    with open(t2gPath) as t2gFh:
+        for line in t2gFh:
+            lineLs = line.split()
+            if (not t2cTrSet) | (lineLs[0] in t2cTrSet):
+                t2gDt[lineLs[0]] = lineLs[1]
+                trLs.append(lineLs[0])
+            else:
+                pass
+    geneLs = list(set(t2gDt.values()))
+    return t2gDt, trLs, geneLs
+
+
+def parseMatEc(ecPath, t2gDt, trLs):
+    """
+    解析表达矩阵ec
+    
+    ecPath:
+        kb-python 产生的bus文件
+    t2gDt:
+        parseBustoolsIndex 产生的字典 key为转录本名 value为基因名
+    trLs:
+        parseBustoolsIndex 产生的list 所有的转录本 sorted
+    
+    return:
+        函数 输入ec 输出对应的geneLs
+    """
+    ecsDt = {}
+    with open(ecPath) as ecFh:
+        for line in ecFh:
+            l = line.split()
+            ec = int(l[0])
+            trs = [int(x) for x in l[1].split(',')]
+            ecsDt[ec] = trs
+
+    def __ec2g(ec):
+        """
+        函数 输入ec 输出对应的geneLs
+        """
+        if ec in ecsDt:
+            return set(t2gDt[trLs[t]] for t in ecsDt[ec])
+        else:
+            return set([])
+
+    return __ec2g
+
+
+def getBustoolsMappingResult(t2gPath, ecPath, busPath, method='inner', filterUmi=False):
+    """
+    解析kbpython结果 并获得表达情况
+    
+    t2gPath:
+        kbpython index t2g 
+    ecPath:
+        kbpython matrix ec
+    busPath:
+        kbpython bus
+    method:
+        inner|outer  inner指与多个基因比对上取交集 outer指取并集
+    filterUmi:
+        是否只保留仅比对上一个基因的umi
+    return:
+        df:
+            columns = ["barcode", "umi", "geneSet"]
+    """
+    logger.info("start parse index")
+    t2gDt, trLs, geneLs = parseBustoolsIndex(t2gPath)
+    
+    logger.info("start parse mat")
+    ec2GeneFun = parseMatEc(
+        ecPath,
+        t2gDt, trLs)
+
+    busFh = StringIO()
+    
+    logger.info("start parse bus")
+    sh.bustools.text('-p', busPath, _out = busFh)
+
+    busFh = StringIO(busFh.getvalue())
+
+    busDf = pd.read_csv(
+        busFh, sep="\t", header=None, names=["barcode", "umi", "ec", "count"]
+    )
+    
+    logger.info("start get mapped gene")
+    busDf = busDf.assign(geneLs = lambda x:x['ec'].map(ec2GeneFun))
+
+    def __getSetIntersect(*setList):
+        if len(setList) == 1:
+            return setList[0]
+        else:
+            return setList[0].intersection(*setList[1:])
+
+    def __getSetOutersect(*setList):
+        if len(setList) == 1:
+            return setList[0]
+        else:
+            return setList[0].union(*setList[1:])
+    
+    setFc = {'outer':__getSetOutersect,
+            'inner':__getSetIntersect}[method]
+    
+    logger.info("start get finnal results")
+
+    busDf = (
+        busDf.groupby(["barcode", "umi"])["geneLs"]
+        .agg(lambda x: setFc(*x))
+        .reset_index()
+    ).assign(
+        barcodeUmi = lambda df:df['barcode'] + '_' + df['umi']
+    )
+    logger.info(f"before filter {len(busDf)} Umis found")
+    if filterUmi:
+        logger.info("Umi filter: True, start filter")
+        busDf = (
+            busDf.assign(geneCounts=lambda df: df["geneLs"].map(len))
+            .query("geneCounts == 1")
+            .assign(geneLs=lambda df: df["geneLs"].map(lambda x: list(x)[0]))
+        ).drop("geneCounts", axis=1)
+        
+        logger.info(f"after filter {len(busDf)} Umis found")  
+    else:
+        logger.info("Umi filter: False")
+    return busDf
+
+
+def getAdataFromKbNucleiResult(t2gPath, ecPath, splicePath, unsplicePath, needUmiMappingInfo=False, adataPath=False):
+    """
+    用于从kb的nuclei策略中获得adata
+    t2gPath:
+        kbpython index t2g 
+    ecPath:
+        kbpython matrix ec
+    splicePath:
+        kbpython splice bus
+    unsplicePath:
+        kbpython unsplice bus
+    needUmiMappingInfo:.
+        need umi mapping info or not
+    adataPath:
+        adata store path
+
+    return:
+        adata
+        (umiMappingDf)
+    """
+    logger.info("start parse splice bus")
+    spliceBusDf = getBustoolsMappingResult(
+        t2gPath, ecPath, splicePath, "inner", True
+    )
+    logger.info("start parse unsplice bus")
+    unspliceBusDf = getBustoolsMappingResult(
+        t2gPath, ecPath, unsplicePath, "inner", True
+    )
+    kbDf = pd.concat([spliceBusDf, unspliceBusDf])
+
+    logger.info("start get overlap umi")
+    kbUmiGeneCountsSr = kbDf.groupby("barcodeUmi")["geneLs"].agg('count')
+    unoverlapUmiLs = list(kbUmiGeneCountsSr.pipe(lambda x:x[x==1]).index) 
+    overlapUmiSt = set(kbUmiGeneCountsSr.pipe(lambda x:x[x!=1]).index) 
+    overlapUseSr = (
+        kbDf.query("barcodeUmi in @overlapUmiSt")
+        .groupby("barcodeUmi")["geneLs"]
+        .apply(lambda df: True if df.iat[0] == df.iat[1] else False)
+    )
+    overlapUmiUseLs = list(overlapUseSr.pipe(lambda x: x[x]).index)
+    useUmiLs = sorted([*unoverlapUmiLs, *overlapUmiUseLs])
+
+    logger.info("start filter overlap umi and creat anndata")
+    kbDf = kbDf.drop_duplicates('barcodeUmi').query("barcodeUmi in @useUmiLs")
+    kbMtxDf = kbDf.groupby('barcode')['geneLs'].apply(lambda df:df.value_counts()).unstack().fillna(0)
+    kbSplicedMtxDf = spliceBusDf.query("barcodeUmi in @unoverlapUmiLs").groupby("barcode")["geneLs"].apply(
+        lambda df: df.value_counts()
+    ).unstack().reindex(kbMtxDf.index).reindex(kbMtxDf.columns, axis=1).fillna(0)
+    kbUnsplicedMtxDf = unspliceBusDf.query("barcodeUmi in @unoverlapUmiLs").groupby("barcode")["geneLs"].apply(
+        lambda df: df.value_counts()
+    ).unstack().reindex(kbMtxDf.index).reindex(kbMtxDf.columns, axis=1).fillna(0)
+
+    kbAd = creatAnndataFromDf(kbMtxDf, spliced=kbSplicedMtxDf, unspliced=kbUnsplicedMtxDf)
+
+    if adataPath:
+        logger.info("start write anndata")
+        kbAd.write(adataPath)
+    
+    if needUmiMappingInfo:
+        return (kbAd, kbDf)
+    else:
+        return kbAd
+
+
+######
+#cellranger
+#####
+def extractReadCountsByUmiFromTenX(molInfoPath, filtered=True):
+    '''
+    用于从cellRanger文件中提取read counts
+    
+    molInfoPath:
+        molecule_info.h5
+    filtered:
+        只使用过滤后的
+    
+    return:
+        dataframe: columns = ['barcodeUmi', 'featureName', 'readCount']
+    
+    '''
+    def NumToSeq():
+        numToBase = {'00': 'A', '01': 'C', '10': 'G', '11': 'T'}
+
+        def _numToSeq(num):
+            num = int(num)
+            numStr = f'{num:020b}'
+            return ''.join(
+                [numToBase[numStr[2 * x:2 * x + 2]] for x in range(10)])
+
+        return _numToSeq
+
+    numToSeq = NumToSeq()
+    molInfo = h5py.File(molInfoPath, 'r')
+    allBarcode = molInfo['barcodes'][()].astype(str)
+    allFeature = molInfo['features/id'][()].astype(str)
+
+    allUmiCount = pd.DataFrame(np.c_[molInfo['umi'][()], \
+        allBarcode[molInfo['barcode_idx'][()]], \
+            molInfo['count'][()], \
+                allFeature[molInfo['feature_idx'][()]]])
+    if filtered:
+        allUmiCount = allUmiCount[allUmiCount[1].isin(
+            allBarcode[molInfo['barcode_info/pass_filter'][()][:, 0]])]
+    allUmiCount[0] = allUmiCount[0].map(numToSeq)
+    allUmiCount['barcodeUmi'] = allUmiCount[1] + '_' + allUmiCount[0]
+    allUmiCount = allUmiCount.reindex(['barcodeUmi', 3, 2], axis=1, copy=False)
+    allUmiCount.columns = ['barcodeUmi', 'featureName', 'readCount']
+    return allUmiCount
