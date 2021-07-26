2bc60b2a112c9c707e3c5bcdca473b3c66749301
liuzj <liuzj039@gmail.com>
2021-07-26 11:30:53 +0800

    debug: qdel

--- jModule/jpy_qdel.py
+++ jModule/jpy_qdel.py
@@ -32,7 +32,7 @@ if not snakemake mode, PBS_ID is pbsId; else snakemake logpath
             sh.Command("/usr/local/torque/bin/qdel")(pbs_id)
             print(f"qdel {pbs_id}")
         except:
-            sh.Command("/opt/ibm/lsfsuite/lsf/10.1/linux2.6-glibc2.3-x86_64/bin/bkill")pbs_id)
+            sh.Command("/opt/ibm/lsfsuite/lsf/10.1/linux2.6-glibc2.3-x86_64/bin/bkill")(pbs_id)
             print(f"bkill {pbs_id}")
     else:
         # pbs_id = pbs_id.split('/')[-1]
